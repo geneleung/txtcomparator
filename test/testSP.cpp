3441fb3a3b1878f7dc32e02c5eb61d11085f2dec
liangzhen <liangzhen.cn@gmail.com>
2014-03-08 21:49:38 +0800

    添加基本功能智能指针支持，修改自google源码

---
+++ test/testSP.cpp
@@ -0,0 +1,54 @@
+#include "stddefs.h"
+
+#define LOG_TAG "TEST_SP"
+
+using namespace android;
+
+class Obj:public virtual RefBase
+{
+public:
+    Obj(int n):m(n)
+	{
+	    LOGD("%d obj construct!",m);
+	}
+    virtual ~Obj()
+	{
+	    LOGD("destroy %d obj",m);
+	}
+    virtual void onFirstRef()
+	{
+	    LOGD("%d first time construct",m);
+	}
+private:
+    int m;
+};
+int testSP()
+{
+    {				// block 1
+    sp<Obj> o1 = NULL;
+	{			     // block 2
+	    sp<Obj> o2 = new Obj(2); // when exit block 2,o2 should be destroy!
+	    sp<Obj> o3 = new Obj(3);
+
+	    o1 = o3;		// when exit block 2,o3 should not be destroy!
+	    LOGD("leaving block 2...");
+	}
+	LOGD("leaving block 1..."); // when exit block 1,o3 should be destroy!
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
